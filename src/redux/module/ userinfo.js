cd2ecd0bbbe4f2519c0ffa5e749700f65211c492
Doripop <eodnjs72480059@gmail.com>
2022-06-15 19:28:39 +0900

    ㅇㅇㅁㅇㅁ

---
+++ src/redux/module/ userinfo.js
@@ -0,0 +1,37 @@
+//로그인 성공시 나오는 userid+ 이메일 저장
+// 로그인 성공 하고 반환 받은 값은 바로 세션에....
+
+
+
+const CHECK = "user/CHECK"
+
+
+const initialState = {
+    list: [],
+  };
+
+
+  export function userCheck(user_id) {
+    console.log("이것도 안댕?", user_id)
+    return { type: CHECK, user_id };
+  }
+
+
+
+
+export default function reducer(state = initialState, action = {}) {
+    switch (action.type) {
+
+        case "user/CHECK": {
+            const new_user_list = [...state.list, {id : action.user_id}];
+            console.log("들어왔으면 제발 ", new_user_list)
+            return { list: new_user_list };
+          }
+
+
+
+
+        default:
+            return state;
+    }
+}
